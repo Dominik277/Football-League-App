10f3bc3979cd5c9decc867d9b626ed56a37429da
Dominik277 <dominikdoric277@gmail.com>
2021-01-12 14:15:24 +0100

    Vizualni dio za NovostiActivity.

---
+++ app/src/main/java/hr/dominik/footballleagueapp/NovostiActivity.java
@@ -0,0 +1,14 @@
+package hr.dominik.footballleagueapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovostiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novosti);
+    }
+}
