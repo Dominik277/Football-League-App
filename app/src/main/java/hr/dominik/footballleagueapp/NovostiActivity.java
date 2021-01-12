a981a7207d834b931e0908da56b8b8b9a8634494
Dominik277 <dominikdoric277@gmail.com>
2021-01-12 14:21:54 +0100

    Ispravljen vizualni dio za activity_novosti.xml te dodana logika za dodavanje cardView-ova u activity novosti.

--- app/src/main/java/hr/dominik/footballleagueapp/NovostiActivity.java
+++ app/src/main/java/hr/dominik/footballleagueapp/NovostiActivity.java
@@ -1,14 +1,40 @@
 package hr.dominik.footballleagueapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class NovostiActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<ModelNovosti> modelNovostiArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novosti);
+        recyclerView = findViewById(R.id.recyclerViewNovosti);
+
+        modelNovostiArrayList = new ArrayList<>();
+        modelNovostiArrayList.add(new ModelNovosti(R.drawable.ic_baseline_sensors_24,"","",""));
+        modelNovostiArrayList.add(new ModelNovosti(R.drawable.ic_baseline_sensors_24,"","",""));
+        modelNovostiArrayList.add(new ModelNovosti(R.drawable.ic_baseline_sensors_24,"","",""));
+        modelNovostiArrayList.add(new ModelNovosti(R.drawable.ic_baseline_sensors_24,"","",""));
+        modelNovostiArrayList.add(new ModelNovosti(R.drawable.ic_baseline_sensors_24,"","",""));
+        modelNovostiArrayList.add(new ModelNovosti(R.drawable.ic_baseline_sensors_24,"","",""));
+        modelNovostiArrayList.add(new ModelNovosti(R.drawable.ic_baseline_sensors_24,"","",""));
+        modelNovostiArrayList.add(new ModelNovosti(R.drawable.ic_baseline_sensors_24,"","",""));
+
+        NovostiAdapter novostiAdapter = new NovostiAdapter(this,modelNovostiArrayList);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
+                this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(novostiAdapter);
+
     }
 }
